a3481b7c4e0180b77524dd3e7add52a78a3f7947
Adam <gelakinetic@gmail.com>
2015-09-13 16:19:55 -0400

    Added GPLv3 headers

--- Firmware/PixelShirtV2/PixelShirtV2.cpp
+++ Firmware/PixelShirtV2/PixelShirtV2.cpp
@@ -1,3 +1,20 @@
+/* Copyright 2015 Adam Feinstein
+ *
+ * This file is part of PixelShirtV2.
+ *
+ * PixelShirtV2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PixelShirtV2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with PixelShirtV2.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "ArduinoGame.h"
 
 #include "Pong.h"
--- Firmware/PixelShirtV2/Pong.h
+++ Firmware/PixelShirtV2/Pong.h
@@ -1,8 +1,18 @@
-/*
- * Pong.h
+/* Copyright 2015 Adam Feinstein
  *
- *  Created on: Oct 27, 2013
- *      Author: adam
+ * This file is part of PixelShirtV2.
+ *
+ * PixelShirtV2 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PixelShirtV2 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with PixelShirtV2.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef PONG_H_
